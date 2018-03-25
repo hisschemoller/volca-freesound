d537242861026cc0832385477d2874ed1933b448
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-03-25 12:19:17 +0200

    React-starter-kit added.

---
+++ src/routes/home/index.js
@@ -0,0 +1,33 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import Home from './Home';
+import Layout from '../../components/Layout';
+
+async function action({ fetch }) {
+  const resp = await fetch('/graphql', {
+    body: JSON.stringify({
+      query: '{news{title,link,content}}',
+    }),
+  });
+  const { data } = await resp.json();
+  if (!data || !data.news) throw new Error('Failed to load the news feed.');
+  return {
+    title: 'React Starter Kit',
+    chunks: ['home'],
+    component: (
+      <Layout>
+        <Home news={data.news} />
+      </Layout>
+    ),
+  };
+}
+
+export default action;
