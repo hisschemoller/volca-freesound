b31f04a229c7f62b2f49d8244912db8983566825
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-05-24 12:26:42 +0200

    Navigation in header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,12 +11,14 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
 import Link from '../Link';
+import Navigation from '../Navigation';
 
 class Header extends React.Component {
   render() {
     return (
       <div className={s.root}>
         <div className={s.container}>
+          <Navigation />
           <Link className={s.brand} to="/">
             <span className={s.brandTxt}>Volca Freesound</span>
           </Link>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,7 +8,6 @@
  */
 
 import React from 'react';
-import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Navigation.css';
 import Link from '../Link';
@@ -17,6 +16,14 @@ class Navigation extends React.Component {
   render() {
     return (
       <div className={s.root} role="navigation">
+        <Link className={s.link} to="/">
+          Transfer
+        </Link>
+        <span className={s.spacer}> | </span>
+        <Link className={s.link} to="/help">
+          Help
+        </Link>
+        <span className={s.spacer}> | </span>
         <Link className={s.link} to="/about">
           About
         </Link>
