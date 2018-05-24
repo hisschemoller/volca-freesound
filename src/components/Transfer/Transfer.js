23ff47d7f207097be5499036bf363be855790318
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-05-24 16:57:34 +0200

    Transfer component to transfer audio to the Volca.

---
+++ src/components/Transfer/Transfer.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import compose from 'recompose/compose';
+import PropTypes from 'prop-types';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Transfer.css';
+import { pause, start } from '../../actions/volca.actions';
+import downloadReceipt from '../../actions/downloadReceipt.actions';
+import fetchRandomSound from '../../actions/fetchRandomSound.actions';
+
+class Transfer extends React.Component {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    isPaused: PropTypes.bool.isRequired,
+    isStarted: PropTypes.bool.isRequired,
+    position: PropTypes.number.isRequired,
+    showReceipt: PropTypes.bool.isRequired,
+  };
+
+  static defaultProps = {};
+
+  render() {
+    let btnText = 'Start';
+    if (this.props.isStarted) {
+      btnText = this.props.isPaused ? 'Stopping...' : 'Stop';
+    }
+    return (
+      <div>
+        <div className={s.row}>
+          <progress max="1" value={this.props.position} />
+          <span>{Math.round(this.props.position * 100)}%</span>
+          <button
+            onClick={e => {
+              e.preventDefault();
+              if (this.props.isStarted) {
+                this.props.dispatch(pause());
+              } else {
+                this.props.dispatch(start());
+                this.props.dispatch(fetchRandomSound());
+              }
+            }}
+            type="button"
+          >
+            {btnText}
+          </button>
+        </div>
+        <div
+          className={
+            s.receipt + (this.props.showReceipt ? ` ${s.receiptreveal}` : '')
+          }
+        >
+          <span className={s.textline}>
+            File transfer has finished. Please download your receipt.
+          </span>
+          <button
+            onClick={e => {
+              e.preventDefault();
+              this.props.dispatch(downloadReceipt());
+            }}
+            type="button"
+          >
+            Download receipt (text file)
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    isPaused: state.sounds.isPaused,
+    isStarted: state.sounds.isStarted,
+    position: state.sounds.position,
+    showReceipt: !!state.sounds.sounds.allIds.length && !state.sounds.isStarted,
+  };
+}
+
+export default compose(withStyles(s), connect(mapStateToProps))(Transfer);
