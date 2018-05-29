6201d31b5e5efb9d9f91e41b8d2dc37ae7860de1
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-05-29 18:17:12 +0200

    Normalize and Double speed checkboxes.

--- src/components/Sound/Sound.js
+++ src/components/Sound/Sound.js
@@ -4,12 +4,28 @@ import compose from 'recompose/compose';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Sound.css';
+import Row from '../Row';
 
 class Sound extends React.Component {
   static propTypes = {};
 
   render() {
-    return <div />;
+    return (
+      <div>
+        <Row>
+          <label htmlFor="normalize">
+            Normalize
+            <input id="normalize" type="checkbox" />
+          </label>
+        </Row>
+        <Row>
+          <label htmlFor="doubleSpeed">
+            Double speed
+            <input id="doubleSpeed" type="checkbox" />
+          </label>
+        </Row>
+      </div>
+    );
   }
 }
 
