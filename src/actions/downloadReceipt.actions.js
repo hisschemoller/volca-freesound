945467be003a7e4f10bf2b55725e2848da445faa
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-06-14 12:31:15 +0200

    Skip overwritten sounds in receipt text.

--- src/actions/downloadReceipt.actions.js
+++ src/actions/downloadReceipt.actions.js
@@ -1,3 +1,8 @@
+/**
+ * Create a text file and present it to the user for download.
+ * @param {String} filename Name of the file to download
+ * @param {String} text Content of the file.
+ */
 function download(filename, text) {
   const element = document.createElement('a');
   element.setAttribute(
@@ -14,20 +19,37 @@ function download(filename, text) {
   document.body.removeChild(element);
 }
 
+/**
+ * Create the receipt text and file from the received sound's data.
+ */
 export default function downloadReceipt() {
   return (dispatch, getState) => {
-    let txt = `Sounds downloaded from Freesound.org and transferred to the Korg Volca Sample\n\n\n`;
+    let fileContent = '';
+    const header = `Sounds downloaded from Freesound.org and transferred to the Korg Volca Sample\n\n\n`;
     const state = getState();
-    state.sounds.sounds.allIds.forEach(soundID => {
+    const slotIndexes = [];
+    const reversedIDs = [...state.sounds.sounds.allIds].reverse();
+    reversedIDs.forEach(soundID => {
       const sound = state.sounds.sounds.byId[soundID];
-      txt += `Sample slot: ${sound.slotIndex}\n\nSound name: ${
-        sound.name
-      }\nID: ${sound.id}\nDuration: ${sound.duration}\nFile type: ${
-        sound.type
-      }\nLicense: ${sound.license}\nUser name: ${
-        sound.username
-      }\nFreesound URL: ${sound.url}\n\n.............\n\n`;
+
+      // only add the latest transfer to a slot
+      if (slotIndexes.find(sound.slotIndex) === 'undefined') {
+        slotIndexes.push(sound.slotIndex);
+
+        // create the sound information entry
+        const item = `Sample slot: ${sound.slotIndex}\n\nSound name: ${
+          sound.name
+        }\nID: ${sound.id}\nDuration: ${sound.duration}\nFile type: ${
+          sound.type
+        }\nLicense: ${sound.license}\nUser name: ${
+          sound.username
+        }\nFreesound URL: ${sound.url}\n\n.............\n\n`;
+
+        // prepend item, because IDs are reversed
+        fileContent = item + fileContent;
+      }
     });
-    download('volca-freesound-receipt.txt', txt);
+    fileContent = header + fileContent;
+    download('volca-freesound-receipt.txt', fileContent);
   };
 }
