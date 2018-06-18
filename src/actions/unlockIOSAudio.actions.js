4f983225ff6de5a193d5290df3e89ce0b9e437e6
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-06-18 14:31:13 +0200

    IOS audio unlock function moved to action.

---
+++ src/actions/unlockIOSAudio.actions.js
@@ -0,0 +1,29 @@
+import { getAudioContext } from '../components/WebAudio';
+import { initialize } from './volca.actions';
+
+export default function unlockIOSAudio() {
+  return dispatch => {
+    const audioContext = getAudioContext();
+
+    // create an empty buffer
+    const buffer = audioContext.createBuffer(1, 1, 22050);
+    const source = audioContext.createBufferSource();
+    source.buffer = buffer;
+    source.connect(audioContext.destination);
+
+    // play the empty buffer
+    if (typeof source.start === 'undefined') {
+      source.noteOn(0);
+    } else {
+      source.start(0);
+    }
+
+    // setup a timer to wait for audio to run
+    const interval = setInterval(() => {
+      if (audioContext.currentTime > 0) {
+        clearInterval(interval);
+        dispatch(initialize());
+      }
+    }, 100);
+  };
+}
--- src/components/ModalUnlockIOSAudio/ModalUnlockIOSAudio.js
+++ src/components/ModalUnlockIOSAudio/ModalUnlockIOSAudio.js
@@ -3,41 +3,15 @@ import { connect } from 'react-redux';
 import compose from 'recompose/compose';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import { initialize, hideModal } from '../../actions/volca.actions';
+import { hideModal } from '../../actions/volca.actions';
+import unlockIOSAudio from '../../actions/unlockIOSAudio.actions';
 import s from './ModalUnlockIOSAudio.css';
 import Modal from '../Modal';
-import { getAudioContext } from '../WebAudio';
 
 class ModalUnlockIOSAudio extends React.PureComponent {
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
   };
-
-  unlockIOSAudio() {
-    const audioContext = getAudioContext();
-
-    // create an empty buffer
-    const buffer = audioContext.createBuffer(1, 1, 22050);
-    const source = audioContext.createBufferSource();
-    source.buffer = buffer;
-    source.connect(audioContext.destination);
-
-    // play the empty buffer
-    if (typeof source.start === 'undefined') {
-      source.noteOn(0);
-    } else {
-      source.start(0);
-    }
-
-    // setup a timer to wait for audio to run
-    const interval = setInterval(() => {
-      if (audioContext.currentTime > 0) {
-        clearInterval(interval);
-        this.props.dispatch(initialize());
-        this.props.dispatch(hideModal());
-      }
-    }, 100);
-  }
 
   render() {
     const { dispatch } = this.props;
@@ -49,11 +23,12 @@ class ModalUnlockIOSAudio extends React.PureComponent {
         }}
       >
         <div className={s.dialog_content}>
-          <h2>Click</h2>
-          <p>Click to enter the app</p>
+          <h2>Volca Freesound</h2>
+          <p>Click to enter the Volca Freesound app.</p>
           <button
             onClick={() => {
-              this.unlockIOSAudio();
+              dispatch(unlockIOSAudio());
+              dispatch(hideModal());
             }}
           >
             Ok
