c4998f2abb43f4634d5f21cf761a5745e1de0302
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-06-21 17:40:00 +0200

    Help text.

--- src/components/pages/Help/Help.js
+++ src/components/pages/Help/Help.js
@@ -16,7 +16,7 @@ class Help extends React.PureComponent {
               <p className={s.p}>
                 This app downloads random files from the Freesound audio
                 database and transfers them to a Korg Volca Sample connected to
-                the computer's audio output.
+                the computer&apos;s audio output.
               </p>
               <p className={s.p}>
                 The app runs in any reasonably modern browser, desktop or
@@ -24,6 +24,15 @@ class Help extends React.PureComponent {
               </p>
               <p className={s.p} />
               <p className={s.p} />
+            </Section>
+            <Section title="Sample licenses">
+              <p className={s.p}>
+                Please be aware that Freesound samples have different licenses
+                depending on the uploader&apos;s choice. After transfer finishes
+                the app presents a receipt, a text file to download that
+                contains license information and links to the sounds on the
+                Freesound website.
+              </p>
             </Section>
           </Col>
           <Col>
@@ -47,10 +56,10 @@ class Help extends React.PureComponent {
                 </a>.
               </p>
               <p className={s.p}>
-                It plays audio samples, but it can't record them. Audio files
-                are transferred to it from a computer or a mobile device. So
-                it's not a sampler in the proper sense of the word, but a sample
-                player.
+                It plays audio samples, but it can&apos;t record them. Audio
+                files are transferred to it from a computer or a mobile device.
+                So it&apos;s not a sampler in the proper sense of the word, but
+                a sample player.
               </p>
             </Section>
             <Section title="Freesound">
@@ -64,7 +73,7 @@ class Help extends React.PureComponent {
                 </a>{' '}
                 is a collaborative audio database where anyone can upload audio
                 samples and recordings and share them under Creative Commons
-                licenses. It's an initiative of the{' '}
+                licenses. It&apos;s an initiative of the{' '}
                 <a
                   href="https://www.upf.edu/web/mtg"
                   target="_blank"
@@ -85,7 +94,7 @@ class Help extends React.PureComponent {
               </p>
               <p className={s.p}>
                 Freesound has an API to retrieve information and sounds from
-                their database. It's used by this app to find and download
+                their database. It&apos;s used by this app to find and download
                 sounds.
               </p>
             </Section>
@@ -96,23 +105,23 @@ class Help extends React.PureComponent {
           <Col>
             <Section title="Steps to transfer a sample">
               <ol className={s.ol}>
-                <li>Set the computer's volume level close to maximum.</li>
+                <li>Set the computer&apos;s volume level close to maximum.</li>
                 <li>
                   For a neutral signal make sure any sound effects or equalizers
                   on the computer are switched off.
                 </li>
                 <li>
-                  Connect the computer's audio output to the Volca 'Sync in'
-                  input.
+                  Connect the computer&apos;s audio output to the Volca
+                  &apos;Sync in&apos; input.
                 </li>
                 <li>Switch on the Volca.</li>
                 <li>
-                  In the app click the first sample slot, marked '0', to select
-                  it.
+                  In the app click the first sample slot, marked &apos;0&apos;,
+                  to select it.
                 </li>
                 <li>
-                  Press 'Start' on the Volca Freesound page to start the
-                  transfer.
+                  Press &apos;Start&apos; on the Volca Freesound page to start
+                  the transfer.
                 </li>
               </ol>
             </Section>
@@ -123,7 +132,8 @@ class Help extends React.PureComponent {
                 <li>In the app the slot that is loading a sample turns red.</li>
                 <li>The slot turns green when transfer is done.</li>
                 <li>
-                  On the Volca Sample the LED display shows the word 'LOAD'.
+                  On the Volca Sample the LED display shows the word
+                  &apos;LOAD&apos;.
                 </li>
                 <li>Various LEDs on the Volca pulsate while transferring.</li>
               </ul>
@@ -143,10 +153,85 @@ class Help extends React.PureComponent {
         <h2 className={s.h2}>Overview</h2>
         <Grid>
           <Col>
-            <Section title="" />
+            <Section title="">
+              <p className={s.p}>
+                The Volca Sample has 4MB memory that can hold a maximum of 100
+                samples.
+              </p>
+              <p className={s.p}>
+                Any number of the 100 slots can be selected to receive a sample,
+                and because of the limited memory size the maximum length of the
+                random samples can be limited.
+              </p>
+              <p className={s.p}>
+                The app&apos;s screen is divided in 4 sections:
+              </p>
+              <ul className={s.ol}>
+                <li>
+                  <i>Freesound settings</i> - Settings for the random sounds from freesound.org
+                </li>
+                <li>
+                  <i>Audio settings</i> - Change the audio files after they have been downloaded from Freesound.
+                </li>
+                <li>
+                  <i>Slot selection</i> - Which of the slots on the Volca Sample will receive new samples.
+                </li>
+                <li>
+                  <i>File transfer</i> - Starts and stops the download and transfer process.
+                </li>
+              </ul>
+              <p className={s.p} />
+              <p className={s.p} />
+            </Section>
           </Col>
           <Col>
-            <Section title="" />
+            <Section title="Freesound settings">
+              <p className={s.p}>
+                Set <i>Max. duration</i> to limit the maximum length in seconds
+                of the samples that are downloaded from the Freesound database.
+                The number of available samples is shown underneath.
+              </p>
+            </Section>
+            <Section title="Audio settings">
+              <p className={s.p}>
+                <i>Normalize</i> maximizes each sample&apos;s volume before
+                transfer.
+              </p>
+              <p className={s.p}>
+                <i>Double speed</i> doubles each sample&apos;s playback speed,
+                so they use half the memory on the Volca and twice as much
+                sample time can be used. The samples must be pitched down 12
+                steps, one octave, to play them back at their original speed.
+              </p>
+            </Section>
+            <Section title="File transfer">
+              <p className={s.p}>
+                Click <i>Start</i> to start the download and transfer process.
+                When finished a receipt button will appear to download a text
+                file containing license and other information of the transferred
+                samples.
+              </p>
+            </Section>
+            <Section title="Volca Sample slot selection">
+              <p className={s.p}>
+                Initally no slots are selected. There are several ways to select
+                them:
+              </p>
+              <ul className={s.ol}>
+                <li>
+                  Click individual slots in the grid to select or deselect them.
+                </li>
+                <li>
+                  Click &apos;Clear all&apos; or &apos;Select all&apos; to clear
+                  or select all 100 slots.
+                </li>
+                <li>
+                  Select a range of samples by setting the &apos;From&apos; and
+                  &apos;To&apos; fields and clicking the &apos;Set range&apos;
+                  button.
+                </li>
+              </ul>
+            </Section>
           </Col>
         </Grid>
       </div>
