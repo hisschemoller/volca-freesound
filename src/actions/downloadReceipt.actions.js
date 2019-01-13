8cf883cee6e454a3cafe6c77e30eee14ce74b1e0
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2019-01-13 14:04:52 +0100

    MS newlines and use Blob for receipt file.

--- src/actions/downloadReceipt.actions.js
+++ src/actions/downloadReceipt.actions.js
@@ -1,21 +1,29 @@
+const urls = {
+  download: null,
+};
+
 /**
  * Create a text file and present it to the user for download.
  * @param {String} filename Name of the file to download
  * @param {String} text Content of the file.
  */
 function download(filename, text) {
-  const element = document.createElement('a');
-  element.setAttribute(
-    'href',
-    `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`,
-  );
-  element.setAttribute('download', filename);
+  const blob = new Blob([text], { type: 'text/plain;charset=utf-8' });
 
+  // release previous url in case of multiple uses in one session
+  if (urls.download) {
+    window.URL.revokeObjectURL(urls.download);
+  }
+
+  // create an addressable version of the blob
+  urls.download = window.URL.createObjectURL(blob);
+
+  const element = document.createElement('a');
+  element.setAttribute('href', urls.download);
+  element.setAttribute('download', filename);
   element.style.display = 'none';
   document.body.appendChild(element);
-
   element.click();
-
   document.body.removeChild(element);
 }
 
@@ -29,6 +37,7 @@ export default function downloadReceipt() {
     const header = `Sounds downloaded from Freesound.org and transferred to the Korg Volca Sample\n\n\n`;
     const state = getState();
     const reversedIDs = [...state.sounds.sounds.allIds].reverse();
+    const newLine = `\r\n`;
 
     for (let i = 0, n = state.sounds.slotCount; i < n; i += 1) {
       // get the last sound transferred to the current slot
@@ -39,14 +48,19 @@ export default function downloadReceipt() {
       // if found create a text entry for the sound
       if (soundIDForSlot !== undefined) {
         const sound = state.sounds.sounds.byId[soundIDForSlot];
+
         // create the sound information entry
-        const item = `Sample slot: ${sound.slotIndex}\n\nSound name: ${
-          sound.name
-        }\nID: ${sound.id}\nDuration: ${sound.duration}\nFile type: ${
+        const item = `Sample slot: ${
+          sound.slotIndex
+        }${newLine}${newLine}Sound name: ${sound.name}${newLine}ID: ${
+          sound.id
+        }${newLine}Duration: ${sound.duration}${newLine}File type: ${
           sound.type
-        }\nLicense: ${sound.license}\nUser name: ${
+        }${newLine}License: ${sound.license}${newLine}User name: ${
           sound.username
-        }\nFreesound URL: ${sound.url}\n\n.............\n\n`;
+        }\nFreesound URL: ${
+          sound.url
+        }${newLine}${newLine}.............${newLine}${newLine}`;
 
         // append item to text
         fileContent += item;
